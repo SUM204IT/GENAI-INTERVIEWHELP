96656ccfed3573c9e5266eee7afb9f2413fe849b
SUMIT MADDESHIYA <sumitmaddeshiya99@gmail.com>
2026-05-25 23:54:59 +0530

    initial commit

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
+require("dotenv").config();
+const app = require("./src/app")
+const connectToDB = require("./src/config/database")
+
+connectToDB();
+
+app.listen(3000, (req, res) => {
+    console.log("Server is running on port 3000")
+})
